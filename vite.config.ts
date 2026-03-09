ddc7353ac510036ee80c1fa329e6e5d918a9492e
tasohauska <tasohauska@gmail.com>
2026-03-09 15:17:30 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/wais-iv-test/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -22,3 +23,4 @@ export default defineConfig(({mode}) => {
     },
   };
 });
+
